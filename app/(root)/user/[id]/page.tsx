d10cba61247463ce30cf0961ba3b90315c58b225
Mark <marklorenceberon06@gmail.com>
2025-09-22 11:07:36 +0800

    fixed descriptions

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -38,7 +38,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
         </div>
         <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
           <p className="text-30-bold">
-            {session?.id === id ? "Your" : "All"} Startups
+            {session?.id === id ? "Your" : "All"} Devotions
           </p>
           <ul className="card_grid-sm">
             <UserStartups id={id} />
